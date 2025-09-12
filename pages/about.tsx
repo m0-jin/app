dae6cd65ebbf0197a2bf95578f5365090da3666b
m0-jin <141896418+m0-jin@users.noreply.github.com>
2025-09-13 08:58:48 +0900

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -10,11 +10,11 @@ const About = () => {
       <Bar />
       <div className="font-kangwon-light text-xl space-y-8 flex flex-col items-center py-10 border-2 border-purple1 px-8 rounded-lg mt-8 bg-purple1/30">
         <span>* 사용된 기술 : React.js, Next.js, Typescript, TailwindCSS</span>
-        <span>* 개발 기간 : 2022. 8. 15. 하루!</span>
-        <span>* Made by 소영이네 작은 언니</span>
+        <span>* 개발 기간 : 2025. 9. 13. ~ 9.18 </span>
+        <span>* Made by 멍진🧚 </span>
         <button
           onClick={() =>
-            router.push("https://github.com/root-zero-o/soyeong-birthday")
+            router.push("https://github.com/m0-jin/app")
           }
           className="w-[150px] h-[50px] bg-purple2 text-white rounded-xl hover:cursor-pointer hover:bg-purple1/80 transition-all"
         >
