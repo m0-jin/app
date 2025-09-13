beda081c9e52d353237cf3f678dc493dc8dcc163
m0-jin <141896418+m0-jin@users.noreply.github.com>
2025-09-13 09:04:35 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,12 +8,12 @@ const Index = () => {
   const router = useRouter();
   return (
     <div className="wrapper relative">
-      <span className="title py-20 ">🎉김소영 생일 축하합니다🎉</span>
+      <span className="title py-20 ">🎉준혁이의 생일을 축하합니다🎉</span>
       <Bar />
       <div className="w-[500px] pt-20">
-        <Image alt="soyeong-pic" src={image} objectFit="contain" />
+        <Image alt="junhyeok-pic" src={image} objectFit="contain" />
       </div>
-      <span className="font-kangwon-bold pt-4">소영이의 19세 생일 기념</span>
+      <span className="font-kangwon-bold pt-4">준혁이의 마지막 20대 생일 기념</span>
       <div
         onClick={() => router.push("/about")}
         className="w-[70px] h-[70px] rounded-full fixed right-4 bottom-4 bg-purple2 flex items-center justify-center text-white font-kangwon-bold text-lg hover:cursor-pointer hover:-translate-y-2 transition-all"
